2f61a30798538b1f07a1d2353a7297d7e8411ce4
julian22810@gmail.com <julian22810@gmail.com>
2022-11-29 16:44:17 +0100

    supprimer

--- src/main/java/com/example/apcles/ConnexionController.java
+++ src/main/java/com/example/apcles/ConnexionController.java
@@ -7,11 +7,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class ConnexionController {
     @FXML
@@ -36,6 +38,24 @@ public class ConnexionController {
         else {
             welcomeText.setTextFill(Paint.valueOf("red"));
             welcomeText.setText("Le nom d'utilisateur ou le mot de passe rentré n'est pas correcte.");
+        }
+        try (Connection con = DriverManager.getConnection("jdcb:mysql:ap-clefs//localhost:3306/", "root", "")){
+            ListView<clef> listView = new ListView<>();
+            ResultSet results;
+
+            String sql = "SELECT * FROM clef";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            results = stmt.executeQuery();
+            while (results.next()){
+                int id = results.getInt(0);
+                String nom = results.getString(1);
+                String ouvrir = results.getString(2);
+                String nomCouleur = results.getString(3);
+                clef clef = new clef(id, nom, ouvrir, nomCouleur);
+                listView.getItems().add(clef);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
         }
 
     }
---
+++ src/main/java/com/example/apcles/clef.java
@@ -0,0 +1,31 @@
+package com.example.apcles;
+
+public class clef {
+    private final int id;
+    private final String nom;
+    private final String ouvrir;
+    private final String nomCouleur;
+
+    public clef(int id, String nom, String ouvrir, String nomCouleur) {
+        this.id = id;
+        this.nom = nom;
+        this.ouvrir = ouvrir;
+        this.nomCouleur = nomCouleur;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getOuvrir() {
+        return ouvrir;
+    }
+
+    public String getNomCouleur() {
+        return nomCouleur;
+    }
+}
