6e29572e6aa7544367eabf297d35caf5f143bb5a
arenaut <abshol66@gmail.com>
2022-11-29 15:57:38 +0100

    update AjouterController

--- src/main/java/com/example/apcles/AjouterController.java
+++ src/main/java/com/example/apcles/AjouterController.java
@@ -1,7 +1,11 @@
 package com.example.apcles;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,9 +14,11 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.*;
+import java.util.ResourceBundle;
 
-public class AjouterController {
+public class AjouterController implements Initializable {
     @FXML
     private Button ajouter;
     @FXML
@@ -25,15 +31,12 @@ public class AjouterController {
     private TextArea descClef;
     @FXML
     private void ajouter() {
-        try (Connection con = DriverManager.getConnection("jdcb:mysql://localhost:3306/ap-clef", "root", "")){
-            ResultSet results;
-
+        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ap-clefs", "root", "")){
             String sql = "INSERT INTO `clef`( `nom`, `ouvrir`, `nomCouleur`) VALUES (? , ?, ?)";
             PreparedStatement stmt = con.prepareStatement(sql);
             stmt.setString(1, nomClef.getText());
             stmt.setString(2, descClef.getText());
             stmt.setString(3, couleurClef.getValue().toString());
-
             stmt.executeUpdate();
         } catch (SQLException e){
             e.printStackTrace();
@@ -46,5 +49,21 @@ public class AjouterController {
         scene.setTitle("Gestionnaire de Clefs");
         scene.setScene(new Scene(root));
         scene.centerOnScreen();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost/ap-clefs", "root", "")){
+            ResultSet results;
+            String sql = "SELECT * FROM couleur";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            results = stmt.executeQuery();
+            ObservableList<String> options = FXCollections.emptyObservableList();
+            while (results.next()) {
+                options.add(results.getString(0));
+            }
+            couleurClef.getItems().add(options);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
